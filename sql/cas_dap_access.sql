0943b0c1ef79cd6e57f84b2d353cfd5146371142
wumengjun <wumengjun@hfzk-env.com>
2024-01-25 16:05:48 +0800

    修改cas_dap_access数据库初始化语句

--- sql/cas_dap_access.sql
+++ sql/cas_dap_access.sql
@@ -1,3 +1,4 @@
+drop table if exists access_server_port;
 create table access_server_port
 (
     id                int auto_increment comment '主键'
@@ -7,5 +8,4 @@ create table access_server_port
     status            bit         null comment '状态（0-不可用;1-可用）'
 )
     comment '数据接入端口配置';
-use cas_dap_access;
 insert into access_server_port(port, transfer_protocol, status) values(30000, 'HJ212', 1);
